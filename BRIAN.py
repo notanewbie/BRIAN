7266f05c3c715c4f0f6971c312ea5e90207483d2
notanewbie <computerchipcity@aol.com>
2018-07-08 22:27:04 -0400

    Update BRIAN.py

--- BRIAN.py
+++ BRIAN.py
@@ -1,22 +1,68 @@
 import os
 import random
 import math
+import time
+import sys
+from findtext import scour
+botname = "BRIAN"
+ver = "18.7.8"
 #This is BRIAN, a Bot Really Intelligent And Nice.
 #
 #
 #This code is still undergoing Beta testing.
 
+#Function to parse harmful characters.
+
+def parseChars(t):
+    t = t.replace("?", "")
+    t = t.replace('"', "")
+    t = t.replace('PH/', "")
+    t = t.replace('\n', "")
+    t = t.replace('<', "")
+    t = t.replace('>', "")
+    t = t.replace('/', "")
+    t = t.replace(':', "")
+    t = t.replace('*', "")
+    t = t.replace('\\', "")
+    return t
+
+#Log my intelligence upon startup.
+
+#Find out how many phrases I know.
+kno = os.listdir("DB/")
+i = 0
+for item in kno:
+    i = i + 1
+kno = i * 1.0
+i = 0
+#Find out how many phrases I understand.
+und = os.listdir("PH/")
+for item in und:
+    i = i + 1
+und = i * 1.0
+i = 0
+#Calculate my relative knowledge of phrases known to phrases understood.
+try:
+    Think = und/kno
+    Think = Think * 100
+    Think = str(Think) + "%"
+    #Save the log.
+    filename = time.strftime("%Y.%m.%d - %H-%M-%S.txt")
+    Logz = open(filename, "w");
+    Logz.write("Knows " + str(kno) + " phrases. \n Understands " + str(und) + " phrases. \n Knowledge level: " + Think + ".");
+    file.close(Logz);
+except ZeroDivisionError:
+    #Save the log.
+    filename = time.strftime("%Y.%m.%d - %H-%M-%S.txt")
+    Logz = open(filename, "w");
+    Logz.write("Is brand new. Knows nothing.");
+    file.close(Logz);
+
 #Make a new function. This will be the engine.
 def Initiate(user):
     #Look in the database for a match to "User" so I can respond.
     #Do I know how to respond?
-    USER = user.replace("?", "")
-    USER = USER.replace('"', "")
-    USER = USER.replace('PH/', "")
-    USER = USER.replace('\n', "")
-    USER = USER.replace('<', "")
-    USER = USER.replace('>', "")
-    USER = USER.replace('/', "")
+    USER = parseChars(user)
     exists = os.path.isfile("PH/" + USER + ".ph");
     #If I do
     if exists is True:
@@ -25,32 +71,21 @@ def Initiate(user):
         respo = file.read(Respo);
         Respo.close();
         #reply
-        Input = raw_input("BRIAN: " + respo + "\nYOU: ");
+        Speak(respo)
+        Input = raw_input(botname + ": " + respo + "\nYOU: ");
         
     #If I don't
     if exists is False:
-        #Choose a random entry:
-        DB = os.listdir("DB/");
-        One = random.choice(DB);
-        Respo = open("DB/" + One, "r");
-        respo = file.read(Respo);
-        Respo.close();
-        Input = raw_input("BRIAN: " + respo + "\nYOU: ");
-    INPUT = Input.replace("?", "")
-    INPUT = INPUT.replace('"', "")
-    INPUT = INPUT.replace('PH/', "")
-    INPUT = INPUT.replace('\n', "")
-    INPUT = INPUT.replace('<', "")
-    INPUT = INPUT.replace('>', "")
-    INPUT = INPUT.replace('/', "")
+        #Choose a similar response:
+        PHDB = os.listdir("PH/");
+        T = open("PH/" + scour(user, PHDB)[0], "r");
+        respo = file.read(T);
+        T.close();
+        Input = raw_input(botname + ": " + respo + "\nYOU: ");
+    INPUT = parseChars(Input)
     
-    RESPO = respo.replace("?", "")
-    RESPO = RESPO.replace('"', "")
-    RESPO = RESPO.replace('PH/', "")
-    RESPO = RESPO.replace('\n', "")
-    RESPO = RESPO.replace('<', "")
-    RESPO = RESPO.replace('>', "")
-    RESPO = RESPO.replace('/', "")
+    RESPO = parseChars(respo)
+
     DBup = open("DB/" + INPUT + ".ph", "w");
     DBup.write(Input);
     file.close(DBup);
@@ -59,4 +94,14 @@ def Initiate(user):
     file.close(PHup);
     Initiate(Input)
 start = raw_input("YOU: ")
+#Speak() from pySpeak
+def Speak(words):
+    if "win32" in sys.platform or "win64" in sys.platform:
+        os.system('mshta vbscript:Execute("CreateObject(""SAPI.SpVoice"").Speak(""' + parseChars(words) + '"")(window.close)")')
+    if "darwin" in sys.platform:
+        os.system("say " + words)
+#DEMO CODE:
+#
+#Speak("I'm eighteen years old but I'm not done yet. I'm not Molly and I'm not Percocet.")
+
 Initiate(start);
